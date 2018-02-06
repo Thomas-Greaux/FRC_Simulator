113233f5dffa19def5fdcaee527d2170b5b83c8f
Thomas-Greaux <thomas.greaux@etu.unice.fr>
2018-02-06 17:06:09 +0000

    Reading a file

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Main {
+    private static final String IN_FILE = "./res/input.txt";
+
+    public static void main(String[] args) {
+        Reader reader = new Reader(IN_FILE);
+        String line;
+        line = reader.readLine();
+        while(line != null) {
+            System.out.println(line);
+            line = reader.readLine();
+        }
+    }
 }
--- src/Reader.java
+++ src/Reader.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Reader {
+    private String filepath;
+    private BufferedReader br;
+
+    public Reader(String filepath) {
+        this.filepath = filepath;
+        try {
+            br = new BufferedReader(new FileReader(filepath));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String readLine() {
+        String res = null;
+        try {
+            res = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
 }
